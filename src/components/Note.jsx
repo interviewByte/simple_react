33b1f41d855639eac4db0d1e0585bd0923deab1d
Pavan Kumar <pavanrnri1818@gmail.com>
2023-04-08 16:28:26 +0530

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Note = () => {
+  return (
+    <div className='note'>
+        <h1>This is the title</h1>
+        <h1>This is the content</h1>
+    </div>
+  )
+}
+
+export default Note;
